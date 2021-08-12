b62a49d9732cb51987dedd4957d4dbd885928f6c
ombasa <ombasaherman@gmail.com>
2021-08-12 20:35:25 +0300

    models

--- todoApi/models.py
+++ todoApi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+	title = models.CharField(max_length=400)
+	description = models.CharField(max_length=100)
+	completed = models.BooleanField(default=False)
+
